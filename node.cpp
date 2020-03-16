fd583d5d5c44c67c90c90f5e07e159cf5db5f50a
emmashu07 <50778385+emmashu07@users.noreply.github.com>
2020-03-15 20:40:05 -0700

    Add files via upload

---
+++ node.cpp
@@ -0,0 +1,34 @@
+#include "node.h"
+
+Node::Node(int data) {
+	this.data = data;
+	left = NULL;
+	right = NULL;
+}
+
+Node::~Node() {
+}
+
+void Node::setData(int data) {
+	this.data = data;
+}
+
+int Node::getData() {
+	return data;
+}
+
+void Node::setLeft(Node* left) {
+	this.left = left;
+}
+
+Node* Node::getLeft() {
+	return left;
+}
+
+void Node::setRight(Node* right) {
+	this.right = right;
+}
+
+Node* Node::getRight() {
+	return right;
+}
